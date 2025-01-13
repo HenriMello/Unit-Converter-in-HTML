f6e61f7ee4e04c89fed3ce0f4bba8b8355ced15c
Henri_Mello <henri.franca@hotmail.com>
2025-01-13 00:05:16 -0300

    feat: finishing the unit converter

--- main/script.js
+++ main/script.js
@@ -1,14 +1,17 @@
-const inputValue = 10;
-const milesToKm = true;
-const resultElement = document.getElementById('resultElement');
-
-let result = 0;
-
-if (milesToKm === true){
-  result = inputValue * 1.60934;
-} else{
-  result = inputValue / 1.60934;
-}
-const resultString = inputValue + " miles are " + result + " km";
-console.log(resultString);
-resultElement.innerHTML = resultString;
+function convert(){
+    const inputValue = document.getElementById('userInput').value;
+    const unit = document.getElementById("unit").value;
+    const milesToKm = unit === 'milesToKm';
+    const resultElement = document.getElementById('resultElement');
+  
+    let result = 0;
+  
+    if (milesToKm === true){
+      result = inputValue * 1.60934;
+    } else{
+      result = inputValue / 1.60934;
+    }
+    const resultString = inputValue + " milhas são " + result + " km";
+    console.log(resultString);
+    resultElement.innerHTML = resultString;
+  }
